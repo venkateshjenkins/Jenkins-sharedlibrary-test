0a1731e0ced80c6c4116a9b8b861161877206d50
venkateshjenkins <52594646+venkateshjenkins@users.noreply.github.com>
2021-07-09 13:23:22 +0530

    Update tele.groovy

--- vars/tele.groovy
+++ vars/tele.groovy
@@ -4,10 +4,11 @@ def call(script){
  
 
   def t = new TestClass()
+ stage ("build"){
 println(t.calling())
  println script.JOB_NAME
  println "using the env " + env.JOB_NAME
-
+ }
  
 }
 return this
