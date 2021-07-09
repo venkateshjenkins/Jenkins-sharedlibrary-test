e205fc0681231c7eead31850a6620da7d34e7a30
venkateshjenkins <52594646+venkateshjenkins@users.noreply.github.com>
2021-07-09 13:05:42 +0530

    Update tele.groovy

--- vars/tele.groovy
+++ vars/tele.groovy
@@ -7,11 +7,11 @@ def call(script){
 println(t.calling())
  println script.JOB_NAME
  println "using the env " + env.JOB_NAME
- node{
+
  stage("build 01 from tele"){
     
   bat "date"
   
- }}
+ }
 }
 return this
