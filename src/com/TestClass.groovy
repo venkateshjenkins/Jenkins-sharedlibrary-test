f0194a36f01db48d102356265670388ba00c56c7
venkateshjenkins <52594646+venkateshjenkins@users.noreply.github.com>
2021-07-11 12:52:11 +0530

    Update TestClass.groovy

--- src/com/TestClass.groovy
+++ src/com/TestClass.groovy
@@ -1,6 +1,12 @@
 package com
 class TestClass {
-
+  def script
+  public TestClass(script){
+  this.script=script
+  }
+  def Callstage(){
+  script.bat "date"
+  }
 def String calling(){
 return "calling from srcss"
 }
