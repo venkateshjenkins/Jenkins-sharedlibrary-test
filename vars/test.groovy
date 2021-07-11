ff9ab87ab0021051d818642ddf79721d8a747c96
venkateshjenkins <52594646+venkateshjenkins@users.noreply.github.com>
2021-07-11 13:16:36 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,16 +1,16 @@
 import com.TestClass
 def call(Closure cls){
- node{
+
 cls()
  def t = new TestClass(this)
   tele(this)
   
   stage("new"){
-   step{
+  
     script{ t.Callstage() }
    script{ println(t.calling())}
 echo("shred lib")
-   }  }}}
+  }}
 
 def experiment(){
 echo "Testing With Experiment : call another function"
